960471a3b60f2a4903541868e9ab9d59201207a0
Ken-kichi <PoTi15673cmgpjwp@yahoo.co.jp>
2024-06-26 20:01:53 +0900

    update base url

--- app/components/AuthForm.tsx
+++ app/components/AuthForm.tsx
@@ -15,7 +15,8 @@ const AuthForm = () => {
     view='magic_link'
     showLinks={false}
     providers={[]}
-    redirectTo="http://localhost:3000/auth/callback"
+    // redirectTo="http://localhost:3000/auth/callback"
+    redirectTo="https://practice-nextjs-supabase.vercel.app/auth/callback"
     appearance={{ theme: ThemeSupa }}
     />
 
